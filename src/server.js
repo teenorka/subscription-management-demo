e51a1462c2c13555e13ddd04dafafb1eab93b92b
teenorka <teenorka@icloud.com>
2026-07-24 15:14:29 +0200

    feat: add application entrypoint

---
+++ src/server.js
@@ -0,0 +1,22 @@
+import { createApp } from './app.js';
+import { loadConfig } from './config.js';
+import { createDatabase } from './database.js';
+
+const config = loadConfig();
+const database = createDatabase(config.DATABASE_PATH);
+const app = createApp({ database });
+
+const server = app.listen(config.PORT, () => {
+  console.info(`Subscription API listening on port ${config.PORT}`);
+});
+
+function shutdown(signal) {
+  console.info(`${signal} received, shutting down`);
+  server.close(() => {
+    database.close();
+    process.exit(0);
+  });
+}
+
+process.on('SIGINT', () => shutdown('SIGINT'));
+process.on('SIGTERM', () => shutdown('SIGTERM'));
